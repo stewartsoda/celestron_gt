5ed2253ca3670bf3da7cff814d5e5c60079e32f3
Cameron Stewart <944568+stewartsoda@users.noreply.github.com>
2024-12-19 22:08:09 -0700

    incremental changes

--- firmware/nexstar-mc/src/main.cpp
+++ firmware/nexstar-mc/src/main.cpp
@@ -1,11 +1,42 @@
 #include "Arduino.h"
+#include <Adafruit_DotStar.h>
+
+
+
+/* Globals */
+Adafruit_DotStar led(DOTSTAR_NUM, PIN_DOTSTAR_DATA, PIN_DOTSTAR_CLK, DOTSTAR_BRG);
+uint8_t brightness = 0;
+bool goingup=true;
+
 
 void setup()
 {
+  led.begin();
+  led.setBrightness(0);
+  led.setPixelColor(0,0x000000FF);
+  led.show();
 
 }
 
 void loop()
 {
+  if(brightness == 255) {
+    goingup = false;
+    delay(500);
+  }
+  if(brightness == 0) {
+    goingup = true;
+    delay(1000);
+  }
+  if(goingup) {
+    brightness++;
+    delay(5);
+  }
+  else {
+    brightness--;
+    delay(7);
+  }
+  led.setBrightness(brightness);
+  led.show();
 
 }
