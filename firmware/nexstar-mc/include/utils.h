83d0be94cf7c278d7a69ba525d55ce6c9c982690
Cameron Stewart <944568+stewartsoda@users.noreply.github.com>
2024-12-27 07:19:52 -0700

    Solid skeleton for command receiving. Builds. Finally unscrewed the multiple-file and includes issues.

--- firmware/nexstar-mc/include/utils.h
+++ firmware/nexstar-mc/include/utils.h
@@ -3,5 +3,10 @@
 
 
 //Function prototypes
+uint8_t generateChecksum(char *message);
+uint8_t checkChecksum(char *message, char checksum);
+uint8_t decodeCelestronMessage(char *message);
+uint8_t handleCommand(char source, char dest, char command, char* data);
+uint8_t sendCommand(char source, char dest, char command, char *data);
 
 #endif
